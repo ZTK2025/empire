3bf9fbb3a985ddee1fd13e8206fc37e97fc193cd
ZTK2025 <ztkjperez2025@gmail.com>
2025-11-03 23:30:48 -0600

    Add files via upload

--- 127.0.0.1record_stats.php
+++ 127.0.0.1record_stats.php
@@ -1,18 +1,4 @@
 <?php
-// 127.0.0.1record_stats.php - Proxy para GitHub Pages
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
-
-// Simular respuesta exitosa
-$response = array(
-    "status" => "success",
-    "message" => "Stats recorded via PHP proxy",
-    "timestamp" => time(),
-    "player" => "ZTK",
-    "session" => "empire_game"
-);
-
-echo json_encode($response);
+echo json_encode(["status"=>"success","message"=>"Stats recorded"]);
 ?>
